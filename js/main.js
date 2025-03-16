674fe263270c41d31db4ab656a5439b72f67c220
giorgiameffe <giorgiameffe03@gmail.com>
2025-03-16 17:27:30 +0100

    feat: add for loop

--- js/main.js
+++ js/main.js
@@ -2,12 +2,29 @@
 // https://lanciweb.github.io/demo/api/pictures/
 // sfruttando la risposta per generare dinamicamente in pagina una serie di foto
 
+// inizializzare la variabile contenente API pictures
 const uri = 'https://lanciweb.github.io/demo/api/pictures/';
 
+ // fare la chiamata API
 axios.get(uri)
     .then((response) => {
         console.log(response);
 
+        // ottenere l'array di oggetti e salvarlo in una variabile
+        const arrayElement = response.data;
+        console.log(arrayElement);
+
+        // iterare l'array
+        for (let i = 0; i < arrayElement.length; i++) {
+            
+            // ottenere il singolo oggetto e salvarlo in una variabile
+            const currentArray = arrayElement[i];
+            console.log(currentArray);
+            
+            // ottenere l'url delle immagini e salvarlo in una variabile
+            const {url: imageUrl} = currentArray;
+            console.log(imageUrl);
+        }
 
     })
     .catch (error => {
