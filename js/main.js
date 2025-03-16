24236c302e1fdd86441f25be3891afb9f739ddb6
giorgiameffe <giorgiameffe03@gmail.com>
2025-03-16 16:32:11 +0100

    feat: add uri and axios

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log('ciao');
+// Inseriamo un foglio JavaScript ed effettuiamo una chiamata AJAX all’API 
+// https://lanciweb.github.io/demo/api/pictures/
+// sfruttando la risposta per generare dinamicamente in pagina una serie di foto
+
+const uri = 'https://lanciweb.github.io/demo/api/pictures/';
+
+axios.get(uri)
+    .then((response) => {
+        console.log(response);
+
+
+    })
+    .catch (error => {
+        console.log('Errore!');
+    })
+
