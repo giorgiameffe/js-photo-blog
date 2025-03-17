314c67401fa935e9db14a8b3b5ffc21b8fad0293
giorgiameffe <giorgiameffe03@gmail.com>
2025-03-17 19:01:43 +0100

    feat: complete milestone 2

--- js/main.js
+++ js/main.js
@@ -1,46 +1,93 @@
-// Inseriamo un foglio JavaScript ed effettuiamo una chiamata AJAX all’API 
-// https://lanciweb.github.io/demo/api/pictures/
-// sfruttando la risposta per generare dinamicamente in pagina una serie di foto
+// ELEMENTI HTML
+
+// Button
+const buttonElement = document.querySelector('.overlay-button');
+console.log(buttonElement);
+// Overlay
+const overlayElement = document.querySelector('.overlay-container');
+console.log(overlayElement);
+overlayElement.classList.add('d-none');
+
+
+// AXIOS
+
 
 // inizializzare la variabile contenente API pictures
 const uri = 'https://lanciweb.github.io/demo/api/pictures/';
 
- // fare la chiamata API
+// fare la chiamata API
 axios.get(uri)
     .then((response) => {
-        console.log(response);
 
         // ottenere l'array di oggetti e salvarlo in una variabile
         const arrayElement = response.data;
 
         // iterare l'array
         for (let i = 0; i < arrayElement.length; i++) {
-            
+
             // ottenere il singolo oggetto e salvarlo in una variabile
             const currentArray = arrayElement[i];
 
             // ottenere l'url delle immagini e salvarlo in una variabile
-            const {url: imageUrl, date: imageDate, title: imageTitle} = currentArray;
-            console.log(imageUrl,imageDate,imageTitle);
-            
+            const { url: imageUrl, date: imageDate, title: imageTitle } = currentArray;
+
             // prendere elemento html row e salvarlo in una variabile
             const rowElement = document.querySelector('.row');
-            
-            // aggiungere elemento html per creare le card
-            rowElement.innerHTML += ` <div class="col">
-               <img src="img/pin.svg" class="pin-img" alt="Pin rosso">
-               <div class="card-photo">
-                  <img src="${imageUrl}" alt="${imageTitle}"> 
-                </div>
-                <div class="card-info">
-                  <div class= "date">${imageDate}</div>
-                  <h3 class= "title">${imageTitle}</div>
-                </div>
-            </div>`
+
+            // invocare la funzione per creare elemento html
+            rowElement.innerHTML += createHtmlElement(currentArray); 
         }
 
+        // Elemento html (array di card)
+
+        const cardsElement = document.querySelectorAll('.col');
+        console.log(cardsElement);
+
+        // IMMAGINI OVERLAY
+
+        // fare un ciclo for per ottenere le singole card
+
+        for (let i = 0; i < cardsElement.length; i++) {
+
+            const currentCard = cardsElement[i];
+            console.log(currentCard);
+
+            // aggiungere evento al click dell'immagine (overlay)
+
+            currentCard.addEventListener('click', function(){
+
+                overlayElement.classList.remove('d-none');
+            })
+        }
+
+        // aggiungere evento al click del bottone
+
+        buttonElement.addEventListener ('click', function(){
+
+            overlayElement.classList.add('d-none');
+        })
+
+
+
     })
-    .catch (error => {
+    .catch(error => {
         console.log('Errore!');
     })
 
+
+
+// FUNZIONE ELEMENTO HTML
+
+function createHtmlElement(obj) {
+
+    return ` <div class="col">
+<img src="img/pin.svg" class="pin-img" alt="Pin rosso">
+<div class="card-photo">
+   <img src="${obj.url}" alt="${obj.title}"> 
+ </div>
+ <div class="card-info">
+   <div class= "date">${obj.date}</div>
+   <h3 class= "title">${obj.title}</div>
+ </div>
+</div>`
+}
